6ad523f7524552d28aa9d6f2fcfcea5db900ed44
b6020913 <zerotwothirty@live.com>
2017-04-16 18:28:32 +0100

    UI Creation
    Started on UI creation and added MyDialog.cs

--- b6020913/b6020913/Form1.Designer.cs
+++ b6020913/b6020913/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace b6020913
 {
-    partial class Form1
+    partial class Frm_Main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txt_Mode = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_Mode = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.btn_Previous = new System.Windows.Forms.Button();
+            this.btn_Predict = new System.Windows.Forms.Button();
+            this.btn_Empty = new System.Windows.Forms.Button();
+            this.btn_Empty2 = new System.Windows.Forms.Button();
+            this.btn_Enter = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.configureToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(891, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // configureToolStripMenuItem
+            // 
+            this.configureToolStripMenuItem.Name = "configureToolStripMenuItem";
+            this.configureToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.configureToolStripMenuItem.Text = "Configure";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(867, 269);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.txt_Mode);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(276, 302);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(331, 42);
+            this.panel1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(222, 31);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // txt_Mode
+            // 
+            this.txt_Mode.Location = new System.Drawing.Point(235, 8);
+            this.txt_Mode.Name = "txt_Mode";
+            this.txt_Mode.Size = new System.Drawing.Size(84, 20);
+            this.txt_Mode.TabIndex = 1;
+            this.txt_Mode.Text = "Multi-Press";
+            this.txt_Mode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.btn_Enter);
+            this.panel2.Controls.Add(this.btn_Empty2);
+            this.panel2.Controls.Add(this.btn_Empty);
+            this.panel2.Controls.Add(this.btn_Predict);
+            this.panel2.Controls.Add(this.btn_Previous);
+            this.panel2.Controls.Add(this.btn_Next);
+            this.panel2.Controls.Add(this.btn_Mode);
+            this.panel2.Location = new System.Drawing.Point(276, 351);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(330, 345);
+            this.panel2.TabIndex = 3;
+            // 
+            // btn_Mode
+            // 
+            this.btn_Mode.BackColor = System.Drawing.Color.Cyan;
+            this.btn_Mode.Location = new System.Drawing.Point(19, 15);
+            this.btn_Mode.Name = "btn_Mode";
+            this.btn_Mode.Size = new System.Drawing.Size(66, 59);
+            this.btn_Mode.TabIndex = 0;
+            this.btn_Mode.Text = "Mode";
+            this.btn_Mode.UseVisualStyleBackColor = false;
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.BackColor = System.Drawing.Color.Cyan;
+            this.btn_Next.Location = new System.Drawing.Point(91, 16);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(66, 58);
+            this.btn_Next.TabIndex = 1;
+            this.btn_Next.Text = "Next";
+            this.btn_Next.UseVisualStyleBackColor = false;
+            // 
+            // btn_Previous
+            // 
+            this.btn_Previous.BackColor = System.Drawing.Color.Cyan;
+            this.btn_Previous.Location = new System.Drawing.Point(163, 17);
+            this.btn_Previous.Name = "btn_Previous";
+            this.btn_Previous.Size = new System.Drawing.Size(66, 57);
+            this.btn_Previous.TabIndex = 2;
+            this.btn_Previous.Text = "Previous";
+            this.btn_Previous.UseVisualStyleBackColor = false;
+            // 
+            // btn_Predict
+            // 
+            this.btn_Predict.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Predict.Location = new System.Drawing.Point(235, 17);
+            this.btn_Predict.Name = "btn_Predict";
+            this.btn_Predict.Size = new System.Drawing.Size(66, 57);
+            this.btn_Predict.TabIndex = 3;
+            this.btn_Predict.Text = "Show Prediction";
+            this.btn_Predict.UseVisualStyleBackColor = false;
+            // 
+            // btn_Empty
+            // 
+            this.btn_Empty.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Empty.Location = new System.Drawing.Point(237, 80);
+            this.btn_Empty.Name = "btn_Empty";
+            this.btn_Empty.Size = new System.Drawing.Size(64, 54);
+            this.btn_Empty.TabIndex = 4;
+            this.btn_Empty.UseVisualStyleBackColor = false;
+            // 
+            // btn_Empty2
+            // 
+            this.btn_Empty2.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Empty2.Location = new System.Drawing.Point(237, 140);
+            this.btn_Empty2.Name = "btn_Empty2";
+            this.btn_Empty2.Size = new System.Drawing.Size(64, 54);
+            this.btn_Empty2.TabIndex = 5;
+            this.btn_Empty2.UseVisualStyleBackColor = false;
+            // 
+            // btn_Enter
+            // 
+            this.btn_Enter.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Enter.Location = new System.Drawing.Point(237, 200);
+            this.btn_Enter.Name = "btn_Enter";
+            this.btn_Enter.Size = new System.Drawing.Size(64, 109);
+            this.btn_Enter.TabIndex = 6;
+            this.btn_Enter.Text = "Enter";
+            this.btn_Enter.UseVisualStyleBackColor = false;
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(891, 717);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Frm_Main";
+            this.Text = "Mini Keyboard";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configureToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txt_Mode;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_Enter;
+        private System.Windows.Forms.Button btn_Empty2;
+        private System.Windows.Forms.Button btn_Empty;
+        private System.Windows.Forms.Button btn_Predict;
+        private System.Windows.Forms.Button btn_Previous;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Button btn_Mode;
     }
 }
 
--- b6020913/b6020913/Program.cs
+++ b6020913/b6020913/Program.cs
@@ -16,7 +16,7 @@ namespace b6020913
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frm_Main());
         }
     }
 }
