66d40b88eb7a42329e0c7d07233a07d33bf44ee3
b6020913 <b6020913@my.shu.ac.uk>
2017-04-24 19:48:26 +0100

    More Comments

--- b6020913/b6020913/Form1.cs
+++ b6020913/b6020913/Form1.cs
@@ -16,7 +16,7 @@
     {
         string Str_KeyStrokes;
         bool Bool_First_Visit = true;
-        bool[] Bool_IsButtonPresssed = new bool[0x13];
+        bool[] Bool_IsButtonPressed = new bool[0x13];
         bool Boolean_Requires_Saving = false;
         int Int_My_List_Index;
         ListBox Lst_Global_Listbox = new ListBox();
@@ -66,57 +66,57 @@
 
         private bool Different_Button_Pressed(int Button_Pressed) // Checks to see if another button is pressed
         {
-            return (!this.Bool_First_Visit && !this.Bool_IsButtonPresssed[Button_Pressed]); // Returns if the Bool_First_Visit and Bool_IsButtonPressed both equat to true
+            return (!this.Bool_First_Visit && !this.Bool_IsButtonPressed[Button_Pressed]); // Returns if the Bool_First_Visit and Bool_IsButtonPressed both equat to true
         }
 
-        private void Which_Button(object sender, EventArgs e, Button which_button, ListBox which_listBox, ListBox which_Dictionary, int which_Button_Number)
+        private void Which_Button(object sender, EventArgs e, Button which_button, ListBox which_listBox, ListBox which_Dictionary, int which_Button_Number) // This checks to see which button was pressed
         {
-            if (this.Different_Button_Pressed(which_button.TabIndex))
+            if (this.Different_Button_Pressed(which_button.TabIndex)) // If the Different_Button_Pressed returns something then;
             {
-                this.Within_Timer_Tick(sender, e);
+                this.Within_Timer_Tick(sender, e); // Ticks the Timer named Within_Timer
             }
-            if (!(this.txt_Mode.Text == "Multi-Press"))
+            if (!(this.txt_Mode.Text == "Multi-Press")) // If the Mode text reads 'Multi-Press' then;
             {
-                switch (which_Button_Number)
+                switch (which_Button_Number) // Which button was pressed
                 {
                     case 10:
-                        this.Str_KeyStrokes = this.Str_KeyStrokes + "*";
+                        this.Str_KeyStrokes = this.Str_KeyStrokes + "*"; // Adds a asterisk to Str_KeyStrokes
                         goto Label_Test;
 
                     case 11:
-                        this.Str_KeyStrokes = this.Str_KeyStrokes + "#";
+                        this.Str_KeyStrokes = this.Str_KeyStrokes + "#"; // Adds a hashtag to Str_KeyStrokes
                         goto Label_Test;
                 }
-                this.Str_KeyStrokes = this.Str_KeyStrokes + Convert.ToString(which_Button_Number);
+                this.Str_KeyStrokes = this.Str_KeyStrokes + Convert.ToString(which_Button_Number); //Converts Str_KeyStrokes to string
             }
             else
             {
-                if (!this.Bool_First_Visit)
+                if (!this.Bool_First_Visit) //If Bool_First_Visit is true then;
                 {
-                    this.Within_Timer.Enabled = false;
-                    this.Int_My_List_Index++;
-                    if (this.Int_My_List_Index > (which_listBox.Items.Count - 1))
+                    this.Within_Timer.Enabled = false; // Does not enable the timer
+                    this.Int_My_List_Index++; // Adds to Int_My_List_Index
+                    if (this.Int_My_List_Index > (which_listBox.Items.Count - 1)) // If Int_My_List_Index is bigger than which_listBox items - 1 then;
                     {
-                        this.Int_My_List_Index = 0;
+                        this.Int_My_List_Index = 0; // Sets Int_My_List_Index to 0
                     }
-                    try
+                    try // Tries code and catch any errors
                     {
-                        this.txt_Word.Text = this.txt_Word.Text.Remove(this.txt_Word.Text.Length - 1);
-                        this.txt_Word.AppendText(Convert.ToString(which_listBox.Items[this.Int_My_List_Index]));
-                        this.Within_Timer.Enabled = true;
+                        this.txt_Word.Text = this.txt_Word.Text.Remove(this.txt_Word.Text.Length - 1); // Removes the txt_Word text and minuses 1 from the length
+                        this.txt_Word.AppendText(Convert.ToString(which_listBox.Items[this.Int_My_List_Index])); //Converts which_listBox to a string
+                        this.Within_Timer.Enabled = true; //Enables the timer
                         return;
                     }
-                    catch { }
+                    catch { } // Catches any exceptions
                 }
-                    this.Bool_First_Visit = false;
+                    this.Bool_First_Visit = false; // Sets Bool_First_Visit to false
                     this.Within_Timer.Enabled = true;
                     this.txt_Word.AppendText(Convert.ToString(which_listBox.Items[this.Int_My_List_Index]));
 
                 for (int i = 0; i <= (which_listBox.Items.Count - 1); i++)
                 {
-                    this.Lst_Global_Listbox.Items.Add(Convert.ToString(which_listBox.Items[i]));
+                    this.Lst_Global_Listbox.Items.Add(Convert.ToString(which_listBox.Items[i])); //Adds which_listBox to Lst_Global_Listbox
                 }
-                this.Bool_IsButtonPresssed[which_button.TabIndex] = true;
+                this.Bool_IsButtonPressed[which_button.TabIndex] = true; //Sets Bool_IsButtonPressed to true
                 switch (which_Button_Number)
                 {
                     case 10:
@@ -139,7 +139,7 @@
             this.txt_KeysPressed.Text.Substring(0, 1);
         }
 
-        private void Within_Timer_Tick(object sender, EventArgs e)
+        private void Within_Timer_Tick(object sender, EventArgs e) // Method for the Timer Tick
         {
             try
             {
@@ -156,87 +156,87 @@
             {
                 this.txt_Word.AppendText(Convert.ToString(this.Lst_Global_Listbox.Items[this.Int_My_List_Index]));
             }
-            this.txt_Word.Focus();
-            this.Int_My_List_Index = 0;
-            this.Lst_Global_Listbox.Items.Clear();
+            this.txt_Word.Focus(); //Focuses on the txt_Word box so it can be typed into
+            this.Int_My_List_Index = 0; //Sets Int_My_List_Index to 0
+            this.Lst_Global_Listbox.Items.Clear(); // Clears Lst_Global_Listbox items
             for (int i = 0; i <= 0x12; i++)
             {
-                this.Bool_IsButtonPresssed[i] = false;
+                this.Bool_IsButtonPressed[i] = false;
             }
             }
             catch { }
 
         }
 
-        private void btn_2_Click(object sender, EventArgs e)
+        private void btn_2_Click(object sender, EventArgs e) // If button 2 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_2, this.listBox_2, this.listBox_Dictionary_Key_2, 2);
         }
 
-        private void btn_1_Click(object sender, EventArgs e)
+        private void btn_1_Click(object sender, EventArgs e) // If button 1 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_1, this.listBox_1, this.listBox_Dictionary_Key_1, 1);
         }
-        private void btn_3_Click(object sender, EventArgs e)
+        private void btn_3_Click(object sender, EventArgs e) // If button 3 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_3, this.listBox_3, this.listBox_Dictionary_Key_3, 3);
         }
 
-        private void btn_4_Click(object sender, EventArgs e)
+        private void btn_4_Click(object sender, EventArgs e) // If button 4 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_4, this.listBox_4, this.listBox_Dictionary_Key_4, 4);
         }
 
-        private void btn_5_Click(object sender, EventArgs e)
+        private void btn_5_Click(object sender, EventArgs e) // If button 5 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_5, this.listBox_5, this.listBox_Dictionary_Key_5, 5);
         }
 
-        private void btn_6_Click(object sender, EventArgs e)
+        private void btn_6_Click(object sender, EventArgs e) // If button 6 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_6, this.listBox_6, this.listBox_Dictionary_Key_6, 6);
         }
 
-        private void btn_7_Click(object sender, EventArgs e)
+        private void btn_7_Click(object sender, EventArgs e) // If button 7 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_7, this.listBox_7, this.listBox_Dictionary_Key_7, 7);
         }
 
-        private void btn_8_Click(object sender, EventArgs e)
+        private void btn_8_Click(object sender, EventArgs e) // If button 8 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_8, this.listBox_8, this.listBox_Dictionary_Key_8, 8);
         }
 
-        private void btn_9_Click(object sender, EventArgs e)
+        private void btn_9_Click(object sender, EventArgs e) // If button 9 is clicked then;
         {
             this.Which_Button(sender, e, this.btn_9, this.listBox_9, this.listBox_Dictionary_Key_9, 9);
         }
 
-        private void btn_Hashtag_Click(object sender, EventArgs e)
+        private void btn_Hashtag_Click(object sender, EventArgs e) // If button hashtag is clicked then;
         {
             this.Which_Button(sender, e, this.btn_Hashtag, this.listBox_Hashtag, this.listBox_Dictionary_Key_Hash, 11);
         }
 
-        private void btn_Punctuation_Click(object sender, EventArgs e)
+        private void btn_Punctuation_Click(object sender, EventArgs e) // If button punctuation is clicked then;
         {
             this.Which_Button(sender, e, this.btn_Punctuation, this.listBox_Punctuation, this.listBox_Dictionary_Key_Quote, 10);
         }
 
-        private void btn_Mode_Click(object sender, EventArgs e)
+        private void btn_Mode_Click(object sender, EventArgs e) // If button mode is clicked then;
         {
-            if (this.txt_Mode.Text == "Multi-Press")
+            if (this.txt_Mode.Text == "Multi-Press") //If the text says Multi-Press then;
             {
-                this.txt_Mode.Text = "Prediction";
+                this.txt_Mode.Text = "Prediction"; //Changes the text to Prediction
             }
             else
             {
-                this.txt_Mode.Text = "Multi-Press";
+                this.txt_Mode.Text = "Multi-Press"; //Changes the text to Multi-Press
             }
         }
 
         private void Frm_Main_Load(object sender, EventArgs e)
         {
-            Within_Timer.Interval = Int_Interval_Required;
+            Within_Timer.Interval = Int_Interval_Required; //Sets Within_Timer.Interval as Int_Interval_Required
             for (int intWhichButton = 0; intWhichButton <= 18; intWhichButton++)
                 boolsButtonPressed[intWhichButton] = false;
         }
